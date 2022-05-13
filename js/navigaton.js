da41f170195ef2e6b6ad181bada12a5b9b3252c1
amankp77 <pathak.amanratan.che20@itbhu.ac.in>
2022-05-13 09:58:42 +0530

    added the navigation with animation

--- js/navigaton.js
+++ js/navigaton.js
@@ -1,6 +1,18 @@
 const burg = document.querySelector(".burg-cont")
+const sidebar = document.querySelector(".sidebar")
+const container = document.querySelector(".container")
+const navbox = document.querySelectorAll(".nav__link")
 
 
 burg.addEventListener('click', ()=>{
     burg.classList.toggle('close')
+    sidebar.classList.toggle('open')
+    container.classList.toggle('open')
 })
+
+
+navbox.forEach(nav => {
+        nav.addEventListener('click',()=>{
+            sidebar.classList.toggle('open')
+        })
+});
